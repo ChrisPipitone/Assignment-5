c8f5083300fc90e765864375771abedaa673ae5e
christopher pipitone <Chrispipitone123@gmail.com>
2022-01-10 10:03:34 -0500

    modified:   assignment-5.js

--- assignment-5.js
+++ assignment-5.js
@@ -1,8 +1,3 @@
-//make an event handler that takes an event and a function that all i have to do is create the different functions 
-// and not a bunch of even handlers just events i think like for when the different buttons are pressed and when 
-// the hover happens stuff like that  idk if its the best way to do it and the idea is sorta lost now till i 
-// can actually work on it
-
 //retrieve grid
 const grid = document.querySelector(".mt-5.content");
 
@@ -36,7 +31,6 @@ addRow.addEventListener("click", () => {
 });
 
 // ●	add columns to the grid
-
 
 //retrive col btn
 const addCol = document.querySelector(".btn.btn-dark.my-3.add-col");
@@ -80,6 +74,7 @@ delCol.addEventListener("click", () => {
 grid.addEventListener("click", (target) => {
     const option1 = document.getElementById("o1");
 
+
     if(option1.value == "red")
         target.target.style.background = 'red';
     if(option1.value == "blue")
@@ -87,4 +82,36 @@ grid.addEventListener("click", (target) => {
     if(option1.value == "green")
     target.target.style.background = 'green';
     
+})
+
+// ●	fill all uncolored cells with the currently selected color
+//retrive box array
+const boxArr = document.querySelectorAll(".box");
+
+const whiteBtn =  document.getElementById("wBtn");
+const option2 = document.getElementById("o2");
+
+whiteBtn.addEventListener("click", () =>{
+    boxArr.forEach( (box) => {
+        if(box.style.background == "" || box.style.background == "null" )
+            box.style.background = option2.value;
+    })
+})
+
+// ●	fill all cells with the currently selected color
+const fillAllBtn =  document.getElementById("fillAllBtn");
+const option3 = document.getElementById("o3");
+fillAllBtn.addEventListener("click", () =>{
+    boxArr.forEach( (box) => {
+            box.style.background = option3.value;
+    })
+})
+
+// ●	clear all cells/restore all cells to their original/initial color
+
+// ●	click and hold (mouseover) from a single cell (start) to a different cell (end) 
+//      such that all affected/hovered-over cells from start to end change to the currently 
+grid.addEventListener("drag", (target) => {
+    const option4 = document.getElementById("o4");
+    targer.target.style.background = option4.value;
 })
